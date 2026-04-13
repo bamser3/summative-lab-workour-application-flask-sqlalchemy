7de68ea7a3a77225530da1454ae56b633a1ac36f
bamser <111535201+bamser3@users.noreply.github.com>
2026-04-13 22:11:19 +0300

    Merge pull request #1 from bamser3/ExcerciseModel
    Excercise Model

---
+++ server/models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
+db = SQLAlchemy()
+
+class Exercise(db.Model):
+    __tablename__ = 'exercises'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    category = db.Column(db.String)
+    equipment_needed = db.Column(db.Boolean)
+    
